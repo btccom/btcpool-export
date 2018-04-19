1f85e94395f8c9c8a90fd910726a44822db24f5a
SwimmingTiger <hu60.cn@gmail.com>
2018-04-20 02:50:21 +0800

    修复在chrome浏览器中子账户名排序混乱的问题。

--- js/polyfill.js
+++ js/polyfill.js
@@ -4,3 +4,10 @@ if (!String.prototype.trim) {
     return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
   };
 }
+
+// https://stackoverflow.com/questions/1179366/is-there-a-javascript-strcmp
+if (typeof(String.prototype.localeCompare) === 'undefined') {
+    String.prototype.localeCompare = function(str, locale, options) {
+        return ((this == str) ? 0 : ((this > str) ? 1 : -1));
+    };
+}
